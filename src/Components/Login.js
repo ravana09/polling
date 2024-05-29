129dd05491a8854260c75ceaa00f7c99803f4b9e
ravana09 <pradeep09sathish@gmail.com>
2024-05-29 15:01:49 +0530

    "Updated Login and SignUp components with new UI, added form validation, and implemented Formik for form handling."

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,58 +1,134 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Card, Col, Form, Row } from "react-bootstrap";
+import { Button, Card, Col, Form, Row, Container } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons";
+import "../Components/Login.css";
+import * as yup from "yup";
+import { Formik, ErrorMessage } from "formik";
+
+import LoginImg from "../Images/signUp.jpg";
+import GoogleImg from "../Images/googleImg.png";
+import { Link } from "react-router-dom";
 
 function Login() {
-  let [formData, setFormData] = useState();
+  const [formData, setFormData] = useState({
+    PhoneNumber: "",
+    Password: "",
+  });
+
+  const schema = yup.object().shape({
+    PhoneNumber: yup
+      .string()
+      .matches(/^\d{10}$/, "Enter only 10 digits")
+      .required(),
+    Password: yup
+      .string()
+      .matches(/^[\w\d\W]{8}$/, "Enter only 8 digits")
+      .required("Password is required"),
+  });
+
+  function handleInputChanges(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    // Add your form submission logic here
+    console.log(formData);
+    setFormData({ PhoneNumber: "", Password: "" });
   }
 
   return (
     <>
-      <div className="Body-cointainer">
-        <div>
-            <center>
-          <Row>
-            <Col sm={6} md={3} lg={3} xl={2}></Col>
-            <Col sm={6} md={6} lg={3} xl={2}></Col>
-            <Col sm={6} md={3} lg={3} xl={2}></Col>
+      <div className="Body-container">
+        <Container>
+          <Row className="justify-content-center align-items-center">
+            <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
+              <img className="LoginImages" src={LoginImg} alt="loginImage" />
+            </Col>
+
+            <Formik
+              initialValues={formData}
+              validationSchema={schema}
+              enableReinitialize
+              onSubmit={handleSubmit}
+            >
+              {" "}
+              {({ handleSubmit }) => (
+                <Col xs={12} md={6}>
+                  <Card
+                    className="LoginCard mx-auto"
+                    style={{ maxWidth: "100%", height: "auto" }}
+                  >
+                    <Card.Body>
+                      <Form onSubmit={handleSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                          <Form.Label>Mobile Number</Form.Label>
+                          <Form.Control
+                            type="text"
+                            placeholder="Enter Your Mobile Number "
+                            required
+                            name="PhoneNumber"
+                            value={formData.PhoneNumber}
+                            onChange={handleInputChanges}
+                          />
+                        </Form.Group>
+                        <Form.Group
+                          className="mb-3"
+                          controlId="formBasicPassword"
+                        >
+                          <Form.Label>Password</Form.Label>
+                          <Form.Control
+                            type="password"
+                            placeholder="Enter Your Password"
+                            required
+                            name="Password"
+                            value={formData.Password}
+                            onChange={handleInputChanges}
+                          />
+                        </Form.Group>
+                        <center>
+                          <Button
+                            variant="primary"
+                            type="submit"
+                            className="w-75"
+                          >
+                            Login
+                          </Button>
+                        </center>
+                        <hr />
+                        {/* <center>
+                      {" "}
+                      <a href="">Forget Password </a> <p>OR</p>
+                    </center> */}
+
+                        <div className="OtherLogin">
+                          <Button
+                            variant="light"
+                            className="w-100 mb-2 d-flex align-items-center justify-content-center"
+                            style={{ height: "60px" }}
+                          >
+                            <img
+                              src={GoogleImg}
+                              alt="Google img"
+                              className="GoogleImg me-2"
+                            />
+                            CONTINUE WITH GOOGLE
+                          </Button>
+                        </div>
+                        <div>
+                          <p className="text-center">
+                            Don't have an account?{" "}
+                            <Link to="/signup">Sign Up </Link>{" "}
+                          </p>
+                        </div>
+                      </Form>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )}
+            </Formik>
           </Row>
-          <Card style={{ width: "45rem", height: "25rem" }}>
-            <div>
-                
-            </div>
-            <Card.Body>
-              <Form onSubmit={handleSubmit}>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter Your email"
-                    required
-                  />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Enter Your Password"
-                    required
-                  />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                  Login
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
-          </center>
-        </div>
+        </Container>
       </div>
     </>
   );
--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,9 +1,126 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./SignUp.css"; // Import your CSS file for styling
+import { Form, Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import signUpimg from "../Images/signupCard.jpg";
+import GoogleImg from'../Images/googleImg.png'
 
 function SignUp() {
+  const [showOtpInput, setShowOtpInput] = useState(false);
+
+  function handleSendOTP() {
+    // Logic to send OTP (not implemented in this example)
+    setShowOtpInput(true);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault(); // Prevent default form submission behavior
+    // Add your form submission logic here
+  }
+
   return (
-    <div>SignUp</div>
-  )
+    <>
+      <div className="Body-container">
+        <Container>
+          <Row className="justify-content-center align-items-center">
+            <Col xs={12} md={6} className="d-flex justify-content-center mb-3">
+              <img className="LoginImages" src={signUpimg} alt="loginImage" />
+            </Col>
+
+            <Col xs={12} md={6}>
+              <Card
+                className="LoginCard mx-auto"
+                style={{ maxWidth: "95%", height: "auto" || "95%" }}
+              >
+                <Card.Body>
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label>Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        placeholder="Enter Your Name"
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control
+                        type="email"
+                        placeholder="Enter Your email"
+                        required
+                      />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                      <Form.Label>Mobile Number </Form.Label>
+                      <Form.Group className="mb-3" controlId="formBasicEmail">
+                     
+                      <Row>
+                        <Col sm={9}>
+                          <Form.Control
+                            type="text"
+                            placeholder="Enter Your Email or Mobile Number"
+                            required
+                          />
+                        </Col>
+                        <Col sm={3}>
+                          <Button onClick={handleSendOTP}>Send OTP</Button>
+                        </Col>
+                      </Row>
+                    </Form.Group>
+                    {showOtpInput && (
+                      <Form.Group className="mb-3" controlId="formOtp">
+                        <Form.Label>Enter OTP</Form.Label>
+                        <Form.Control
+                          type="text"
+                          placeholder="Enter OTP"
+                          required
+                        />
+                      </Form.Group>
+                    )}
+                   
+                     
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control
+                        type="password"
+                        placeholder="Enter Your Password"
+                        required
+                      />
+                    </Form.Group>
+                    <center>
+                      <Button variant="primary" type="submit" className="w-50 ">
+                        Sign Up
+                      </Button>
+                    </center>
+                    <hr />
+                    
+
+                    <div className="OtherLogin">
+                      <Button
+                        variant="light"
+                        className="w-100 mb-2 d-flex align-items-center justify-content-center"
+                        style={{ height: "60px" }}
+                      ><img src={GoogleImg} alt="googleImage" className="GoogleImg"></img>
+                        CONTINUE WITH GOOGLE
+                      </Button>
+                    
+                    </div>
+                    <div>
+                      <p className="text-center">
+                        Already have an account? <Link to="/">Sign In</Link>{" "}
+                      </p>
+                    </div>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
 }
 
-export default SignUp
+export default SignUp;
